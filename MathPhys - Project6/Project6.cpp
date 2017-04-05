2b57406d53dd5a9e002a38985bdfe38daf26595b
(Student) - Aaron Curry <C00202505@ITCARLOW.IE>
2017-04-05 16:44:33 +0100

    Added changable air resistance to 2d projectile model and collision with a target, added the abiltiy to record number of attempts until target hit

--- MathPhys - Project6/Project6.cpp
+++ MathPhys - Project6/Project6.cpp
@@ -60,6 +60,7 @@ int main()
 	float degrees = 0.0f;
 	float radians = 0.0f;
 	float force = 0.0f;
+	float airResistance = 0.0f;
 
 	bool printHeight = false;
 	bool timing = false;
@@ -69,31 +70,41 @@ int main()
 	bool slideLeft = false;
 	bool printTime = false;
 	bool reset = false;
+	bool collide = false;
 
 	int count = 0;
 	int timeCount = 0;
+	int attempt = 0;
 
 	double timeSinceLast = 0.0;
 	double startPosition = 400;
 	// setup any shapes or variables.
 	sf::CircleShape player(10);
+	sf::CircleShape target(20);
 	sf::Vertex line[] =
 	{
+		
 		sf::Vertex(sf::Vector2f(0, 420)),
 		sf::Vertex(sf::Vector2f(800, 420))
+		
 	};
 
 	player.setFillColor(sf::Color::Red);
+	target.setFillColor(sf::Color::Blue);
 
 	sf::Vector2f velocity(0, 0);
 	sf::Vector2f position(50, 398);
 	sf::Vector2f initialPosition(0, 0);
 
+	sf::Vector2f targetPos(500, 380);
 	//For use in games
 	float pixelsToMeters = 20;
 	sf::Vector2f gravity(0.0f, 9.8f*pixelsToMeters);
 	sf::Vector2f acceleration(0.0f, 0.0f);
 	sf::Vector2f initialVelocity(0.0f, 0.0f);
+	float distX = 0;
+	float distY = 0;
+	float distBetween = 0;
 
 
 	//Load font
@@ -106,24 +117,34 @@ int main()
 	sf::Text m_DistanceXText;
 	sf::Text m_AngleText;
 	sf::Text m_ForceText;
+	sf::Text m_AttemptText;
+	sf::Text m_HitText;
+	sf::Text m_AirResText;
 
 	sf::Text m_MaxHeight;
 	sf::Text m_TimeInAir;
 	sf::Text m_DistanceX;
 	sf::Text m_Angle;
 	sf::Text m_Force;
+	sf::Text m_Attempt;
+	sf::Text m_AirRes;
 
 	std::stringstream m_sMaxHeight;
 	std::stringstream m_sTimeInAir;
 	std::stringstream m_sDistanceX;
 	std::stringstream m_sAngle;
 	std::stringstream m_sForce ;
+	std::stringstream m_sAttempt;
+	std::stringstream m_sAirRes;
 
 	m_MaxHeightText.setFont(m_font);
 	m_TimeInAirText.setFont(m_font);
 	m_DistanceXText.setFont(m_font);
 	m_AngleText.setFont(m_font);
 	m_ForceText.setFont(m_font);
+	m_AttemptText.setFont(m_font);
+	m_HitText.setFont(m_font);
+	m_AirResText.setFont(m_font);
 
 	m_MaxHeight.setFont(m_font);
 	m_text.setFont(m_font);
@@ -131,11 +152,17 @@ int main()
 	m_DistanceX.setFont(m_font);
 	m_Angle.setFont(m_font);
 	m_Force.setFont(m_font);
+	m_Attempt.setFont(m_font);
+	m_AirRes.setFont(m_font);
 
 	m_sAngle << 0;
 	
 	
 	m_sForce << 0;
+
+	m_sAirRes << 0;
+
+	
 	
 
 	// set timepeFrame to 1 60th of a second. 60 frames per second
@@ -172,14 +199,28 @@ int main()
 		if (timeSinceLastUpdate > timePerFrame)
 		{
 
+			//To check collision between the player and target
+			distX = (targetPos.x - position.x) * (targetPos.x - position.x);
+			distY = (targetPos.y - position.y) * (targetPos.y - position.y);
+
+			distBetween = std::sqrt(distX + distY);
+			//std::cout << distBetween << std::endl;
+
+			if (distBetween <= player.getRadius() + target.getRadius())
+			{
+				std::cout << "collision" << std::endl;
+				collide = true;
+			}
+			
+
+
+
 			//Reset the variables
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::R) && reset == false)
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::B) && reset == false)
 			{
 				bounce = false;
 				slideRight = false;
 				timing = false;
-				degrees = 0;
-				force = 0;
 				velocity.y = 0;
 				velocity.x = 0;
 				position.x = 50;
@@ -187,13 +228,24 @@ int main()
 				distanceY = 0;
 				maxHeight = 0;
 				count = 0;
-				m_sAngle.str("");
-				m_sAngle << 0;
+				
+				/*degrees = 0;
+				force = 0;
+				m_sForce.str("");
+				m_sAngle.str("");*/
+
 				m_sDistanceX.str("");
 				m_sMaxHeight.str("");
-				m_sForce.str("");
-				m_sForce << 0;
+				
 				m_sTimeInAir.str("");
+				
+				if (collide == true)
+				{
+					m_sAttempt.str("");
+					m_sAttempt << 0;
+					attempt = 0;
+					collide = false;
+				}
 
 			}
 			//Key press to increase angle
@@ -222,20 +274,18 @@ int main()
 				m_sAngle << degrees;
 			}
 			//Key press to decrease force
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::V) && bounce == false)
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::F) && bounce == false)
 			{
 				if (force > 0)
 				{
 					force--;
 				}
-				
-				initialVelocity.x = force * cos(radians);
-				initialVelocity.y = force * sin(radians);
+			
 				m_sForce.str("");
 				m_sForce << force;
 			}
 			//Key press to increase force
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::B) && bounce == false)
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::V) && bounce == false)
 			{
 
 				if (force >=0)
@@ -243,11 +293,29 @@ int main()
 					force++;
 				}
 				
-				initialVelocity.x = force * cos(radians);
-				initialVelocity.y = force * sin(radians);
-
 				m_sForce.str("");
 				m_sForce << force;
+			}
+			initialVelocity.x = force * cos(radians);
+			initialVelocity.y = force * sin(radians);
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::X) && bounce == false)
+			{
+				if (airResistance > 0)
+				{
+					airResistance--;
+				}
+
+	
+				m_sAirRes.str("");
+				m_sAirRes << airResistance;
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::C) && bounce == false)
+			{
+				
+					airResistance++;
+			
+					m_sAirRes.str("");
+					m_sAirRes << airResistance;
 			}
 
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && position.y >= 398 && bounce == false)
@@ -269,30 +337,50 @@ int main()
 			}
 
 			//Calculates how much energy is lost in bounce
-			if (bounce == true)
-			{
-				if (position.y >= 400)
-				{
-					velocity.y = velocity.y * -0.6f;
-					acceleration.x = -0.8*9.81* (velocity.x / sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y)));
-					std::cout << "friction" << std::endl;
-					position.y = 400;
-					
-				}
+			//if (bounce == true)
+			//{
+			//	if (position.y >= 400)
+			//	{
+			//		velocity.y = velocity.y * -0.6f;
+			//		position.y = 400;
+			//		
+			//	}
 
+			//	velocity.y = velocity.y + gravity.y * timeSinceLastUpdate.asSeconds();
+			//	// update position and velocity here using equations in lab sheet using timeChange as  timeSinceLastUpdate.asSeconds().
+			//	position.y = position.y + velocity.y * timeSinceLastUpdate.asSeconds() + 0.5 * gravity.y *(timeSinceLastUpdate.asSeconds() * timeSinceLastUpdate.asSeconds());
+			//}
+
+			//stops ball dead 
+			if (position.y >= 400)
+			{
+				if (collide == false  && velocity.y > 0)
+				{
+					attempt = attempt + 1;
+					
+					m_sAttempt.str("");
+					m_sAttempt << attempt;
+				}
+				velocity.y = 0;
+				velocity.x = 0;
+				position.y = 400;
+				slideRight = false;
+
+			}
+			
+			if (slideRight == true)
+			{
 				velocity.y = velocity.y + gravity.y * timeSinceLastUpdate.asSeconds();
 				// update position and velocity here using equations in lab sheet using timeChange as  timeSinceLastUpdate.asSeconds().
 				position.y = position.y + velocity.y * timeSinceLastUpdate.asSeconds() + 0.5 * gravity.y *(timeSinceLastUpdate.asSeconds() * timeSinceLastUpdate.asSeconds());
-			}
-			
-			//// update position and velocity here using equations in lab sheet using timeChange as  timeSinceLastUpdate.asSeconds().
-			if (slideRight == true && position.x >= 1 && position.x < 765)
-			{
-				if (slideRight == true)
+				if (position.x >= 1 && position.x < 765)
 				{
+					acceleration.x = 9.81 - airResistance * (velocity.x / sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y)));
+					
+					//std::cout << "friction" << std::endl;
 					position.x = position.x + velocity.x * timeSinceLastUpdate.asSeconds() + 0.5 * acceleration.x *(timeSinceLastUpdate.asSeconds() * timeSinceLastUpdate.asSeconds());
 					velocity.x = velocity.x + acceleration.x * timeSinceLastUpdate.asSeconds();
-
+					
 					distanceX = position.x - initialPosition.x;
 				}
 
@@ -335,9 +423,12 @@ int main()
 			}
 
 			player.setPosition(position.x, position.y);
+			target.setPosition(targetPos.x, targetPos.y);
 			// clear the screen and draw all the shapes
 
-			window.clear();
+		//	std::cout << velocity.x << std::endl;
+
+			window.clear(sf::Color(150,150,150, 255));
 
 			//Text objects are drawn when the game is playing
 
@@ -353,6 +444,19 @@ int main()
 			m_Angle.setScale(1, 1);
 			m_Angle.setColor(sf::Color::Yellow);
 			window.draw(m_Angle);
+
+			m_AttemptText.setString("Attempt :");
+			m_AttemptText.setPosition(20, 70);
+			m_AttemptText.setScale(1, 1);
+			m_AttemptText.setColor(sf::Color::White);
+			window.draw(m_AttemptText);
+
+			m_Attempt.setString(m_sAttempt.str());
+			m_Attempt.setPosition(140, 70);
+			m_Attempt.setScale(1, 1);
+			m_Attempt.setColor(sf::Color::Cyan);
+			window.draw(m_Attempt);
+
 
 			m_ForceText.setString("Force :");
 			m_ForceText.setPosition(20, 40);
@@ -402,10 +506,33 @@ int main()
 			m_MaxHeight.setColor(sf::Color::Cyan);
 			window.draw(m_MaxHeight);
 
+			m_AirResText.setString(":AirResistance");
+			m_AirResText.setPosition(645, 70);
+			m_AirResText.setScale(0.85, 0.85);
+			m_AirResText.setColor(sf::Color::White);
+			window.draw(m_AirResText);
+
+			m_AirRes.setString(m_sAirRes.str());
+			m_AirRes.setPosition(605, 70);
+			m_AirRes.setScale(0.85, 0.85);
+			m_AirRes.setColor(sf::Color::Yellow);
+			window.draw(m_AirRes);
+
 			window.draw(player);
+			window.draw(target);
 			window.draw(line, 2, sf::Lines);
 
 
+			m_HitText.setString("Target Hit");
+			m_HitText.setPosition(300, 70);
+			m_HitText.setScale(2.00, 2.00);
+			m_HitText.setColor(sf::Color::Red);
+
+			if (collide == true)
+			{
+				window.draw(m_HitText);
+			}
+			
 
 			window.display();
 
